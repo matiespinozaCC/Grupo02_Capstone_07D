f82117b0c759d4ce71f7cfcf7e10529954f50a08
matiespinozaCC <131064240+matiespinozaCC@users.noreply.github.com>
2024-09-24 09:42:59 -0300

    Redirige

--- PlanAway/src/app/login/login.page.ts
+++ PlanAway/src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 // src/app/login/login.page.ts
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router'; // Importar Router
 
 @Component({
   selector: 'app-login',
@@ -11,17 +12,17 @@ export class LoginPage {
   email: string = '';  // Inicializar con una cadena vacía
   password: string = ''; // Inicializar con una cadena vacía
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
     this.authService.login(this.email, this.password)
       .then(user => {
         console.log('Usuario:', user);
         // Redirige o maneja el inicio de sesión exitoso
+        this.router.navigate(['/tabs/tab1']); // Cambia '/home' a la ruta que necesites
       })
       .catch(error => {
         console.error('Error al iniciar sesión:', error);
       });
   }
 }
-  
