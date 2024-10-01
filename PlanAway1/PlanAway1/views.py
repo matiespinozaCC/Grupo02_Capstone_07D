25846b2d7318098f035fda5e51b2030bb4e59fc3
dabrianla <81344554+dabrianla@users.noreply.github.com>
2024-10-01 03:50:12 -0300

    nuevas paginas y arreglos de templates

--- PlanAway1/PlanAway1/views.py
+++ PlanAway1/PlanAway1/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'PlanAway1/templates/login.html')
+
+def login(request):
+    return render(request,'PlanAway1/templates/login.html')
