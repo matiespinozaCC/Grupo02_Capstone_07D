0afa2312c0653aed9d3fe2e7f7a3bc586800c0af
dabrianla <81344554+dabrianla@users.noreply.github.com>
2024-10-02 23:46:55 -0300

    Cambios
    Cambios leves en la firebase

--- PlanAway1/PlanAway/views.py
+++ PlanAway1/PlanAway/views.py
@@ -3,17 +3,6 @@ from django.http import HttpResponse
 import firebase
 # Create your views here.
 
-
- firebaseConfig = {
-  apiKey: "AIzaSyBkb-EcRxHOmeaiwv32RvpbiBqhZmnz7RQ",
-  authDomain: "planaway-440ba.firebaseapp.com",
-  projectId: "planaway-440ba",
-  storageBucket: "planaway-440ba.appspot.com",
-  messagingSenderId: "1013793686325",
-  appId: "1:1013793686325:web:10059ff420ec912f37427c"
-}
-
-
 TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates"),'
 )
@@ -23,3 +12,4 @@ def index(request):
 
 def login(request):
     return render(request, "login.html")
+ 
