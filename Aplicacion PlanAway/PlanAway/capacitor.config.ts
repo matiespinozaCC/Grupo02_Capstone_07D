0d628a04a19bfc21819f2232b4081d6356d2233a
matiespinozaCC <131064240+matiespinozaCC@users.noreply.github.com>
2024-10-08 09:50:20 -0300

    eliminacion antiguo

--- Aplicacion PlanAway/PlanAway/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PlanAway',
-  webDir: 'www'
-};
-
-export default config;
